53a163ad2c0653d081d1d74628bed8f2637083bb
Vish Singh <vishalicious213@gmail.com>
2021-01-13 09:51:40 -0800

    completed day-01-exercises.js

--- day-01-exercises.js
+++ day-01-exercises.js
@@ -14,3 +14,34 @@ let name = 'Vish'
 let hasKids = true
 let noValue // undefined
 let nullValue = null
+
+// DATATYPES.JS
+
+console.log(typeof name)
+console.log(typeof hasKids)
+console.log(typeof noValue)
+console.log(typeof nullValue)
+
+let a
+let b
+let c
+let d
+
+let e = 1
+let f = 2
+let g = 3
+let h = 4
+
+let firstName = 'Vish'
+let lastName = 'Singh'
+let married = 'for now'
+let country = 'USA'
+let age = 45
+
+let fn = firstName, ln = lastName, m = married, c = country, a = age
+
+let myAge = 25
+let yourAge = 45
+
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
