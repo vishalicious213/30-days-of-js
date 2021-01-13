32ebe36114a5dcad67a15d085e33ad3f52a53c6b
Vish Singh <vishalicious213@gmail.com>
2021-01-13 09:04:34 -0800

    drafted day-01-exercises.js

---
+++ day-01-exercises.js
@@ -0,0 +1,16 @@
+// comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/*
+    comments can make code readable,
+    easy to reuse and informative
+*/
+
+
+// VARIABLE.JS
+
+let name = 'Vish'
+let hasKids = true
+let noValue // undefined
+let nullValue = null
